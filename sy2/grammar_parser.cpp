1e0655e41ded683b02db070136981c0bdb965ee4
lan-ce-lot <1617025846@qq.com>
2020-12-30 19:57:01 +0800

    os

--- sy2/grammar_parser.cpp
+++ sy2/grammar_parser.cpp
@@ -5,7 +5,7 @@
 #include<set>
 #include<stack>
 #include <algorithm>
-#include <lexical_1.h>
+#include "../../os/sy5/lexical_1.h"
 
 using namespace std;
 #define debug(a) cout << "*" << a << "*" << endl
